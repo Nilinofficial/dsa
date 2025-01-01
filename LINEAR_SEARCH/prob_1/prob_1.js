eb9ea1562ff410daf1f9b49b349df1aef5f31aa6
Nilin <itsmenilin18@gmail.com>
2025-01-01 19:20:40 +0530

    Add array methods to solve problem

--- LINEAR_SEARCH/prob_1/prob_1.js
+++ LINEAR_SEARCH/prob_1/prob_1.js
@@ -1,28 +1,21 @@
 // Find whether 14 exists in the array or not ?
-
 // arr = [18,12,16,14,17,21,23,24];
-
 const arr = [18, 12, 16, 14, 17, 21, 23, 24];
 const targetElement = 16;
-
-function findTheElement(arr: number[], targetElement: number) {
-  if (arr.length === 0) {
-    return -1;
-  }
-
-  for (let index = 0; index < arr.length; index++) {
-    if (arr[index] === targetElement) {
-      return index;
+function findTheElement(arr, targetElement) {
+    if (arr.length === 0) {
+        return -1;
     }
-  }
-  return -1;
+    for (let index = 0; index < arr.length; index++) {
+        if (arr[index] === targetElement) {
+            return index;
+        }
+    }
+    return -1;
 }
-
 const result1 = findTheElement(arr, targetElement);
 console.log("native", result1);
-
 // alernate methods using array methods
 // findIndex
-
 const result2 = arr.findIndex((element) => element === targetElement);
 console.log("findIndex", result2);
