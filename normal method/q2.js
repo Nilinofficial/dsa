71dc9d050568bdff2ec138fe97815ec5aad90463
Nilinofficial <itsmenilin18@gmail.com>
2025-06-04 22:26:16 +0530

    Add problem 2

---
+++ normal method/q2.js
@@ -0,0 +1,17 @@
+// Write a function that returns the number of negative numbers in an array
+
+let arr = [2,-9,17,0,1,-10,-4,8];
+
+function findNegativeNumbers(array) {
+    let count = 0;
+
+    for(let index = 0 ; index<arr.length; index++){
+            if (array[index]<0){
+                count = count+1;
+            }
+    }
+    return count;
+}
+
+const result = findNegativeNumbers(arr);
+console.log(`The number of negative numbers is ${result}`);
