f0d24c01f7d045e4fb5317f8e4a4cb80040ac569
Nilinofficial <itsmenilin18@gmail.com>
2025-06-04 22:35:43 +0530

    Add problem 3

--- normal method/q1.js
+++ normal method/q1.js
@@ -3,12 +3,12 @@
 const numberArray = [2, 4, 10, 12, 16, 18, 20];
 const targetElement = 16;
 function findIndex(array, element) {
-    for (let index = 0; index < numberArray.length; index++) {
-        if (array[index] === element) {
-            return index;
-        }
+  for (let index = 0; index < numberArray.length; index++) {
+    if (array[index] === element) {
+      return index;
     }
-    return -1;
+  }
+  return -1;
 }
 let result = findIndex(numberArray, targetElement);
 console.log(result);
--- normal method/q2.js
+++ normal method/q2.js
@@ -1,16 +1,16 @@
 // Write a function that returns the number of negative numbers in an array
 
-let arr = [2,-9,17,0,1,-10,-4,8];
+let arr = [2, -9, 17, 0, 1, -10, -4, 8];
 
 function findNegativeNumbers(array) {
-    let count = 0;
+  let count = 0;
 
-    for(let index = 0 ; index<arr.length; index++){
-            if (array[index]<0){
-                count = count+1;
-            }
+  for (let index = 0; index < arr.length; index++) {
+    if (array[index] < 0) {
+      count = count + 1;
     }
-    return count;
+  }
+  return count;
 }
 
 const result = findNegativeNumbers(arr);
